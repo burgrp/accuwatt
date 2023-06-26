bbab5deba348d816f06c98aa8c754951fec10a6c
Pavel Burgr <pb@drake.cz>
2023-06-26 14:07:12 +0200

    call version

--- fw/site_config.py
+++ fw/site_config.py
@@ -3,11 +3,14 @@ wifi_password='ferdamravenec'
 mqtt_broker='10.1.0.12'
 name='accu-test'
 debug=True
-period_ms=500
 wifi_led_pin=12
+
+period_ms=500
+
 temp_address=79
 temp_sda=4
 temp_scl=5
 temp_limit=25
 temp_hysteresis=2
+
 channels = (13,2)
